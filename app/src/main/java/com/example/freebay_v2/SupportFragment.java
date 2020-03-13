756c78859f3cb5ec1b78dcce87ea10c37d057fd6
Max Wright <u1853896@unimail.hud.ac.uk>
2020-03-13 11:11:30 +0000

    Map implemented but crashes

--- app/src/main/java/com/example/freebay_v2/SupportFragment.java
+++ app/src/main/java/com/example/freebay_v2/SupportFragment.java
@@ -5,16 +5,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SupportFragment extends Fragment  {
+public class SupportFragment extends Fragment implements OnMapReadyCallback{
+
+    GoogleMap map;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_support, container, false);
+        View view = inflater.inflate(R.layout.fragment_support, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return view;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng uoh = new LatLng(53.643727, -1.7809775);
+        map.addMarker(new MarkerOptions().position(uoh).title("Headquaters"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(uoh));
+    }
 }
