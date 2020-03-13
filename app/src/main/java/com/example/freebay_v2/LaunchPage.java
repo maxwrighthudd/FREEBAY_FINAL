c5367c20ef6e50cd7bc918c9e7a9ed5f1a347c87
Max Wright <u1853896@unimail.hud.ac.uk>
2020-03-13 10:42:23 +0000

    Login and registration changed to activities

--- app/src/main/java/com/example/freebay_v2/LaunchPage.java
+++ app/src/main/java/com/example/freebay_v2/LaunchPage.java
@@ -1,12 +1,12 @@
 package com.example.freebay_v2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 
 public class LaunchPage extends AppCompatActivity {
     Button regBtn;
@@ -23,11 +23,8 @@ public class LaunchPage extends AppCompatActivity {
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signIn.setVisibility(View.GONE);
-                regBtn.setVisibility(View.GONE);
-                FragmentManager fm = getSupportFragmentManager();
-                RegistrationFragment registrationFragment = new RegistrationFragment();
-                fm.beginTransaction().replace(R.id.homepage, registrationFragment).commit();
+                Intent intent = new Intent(LaunchPage.this, RegistrationPage.class);
+                startActivity(intent);
             }
         });
 
@@ -35,11 +32,8 @@ public class LaunchPage extends AppCompatActivity {
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signIn.setVisibility(View.GONE);
-                regBtn.setVisibility(View.GONE);
-                FragmentManager fm = getSupportFragmentManager();
-                LoginFragment loginFragment = new LoginFragment();
-                fm.beginTransaction().replace(R.id.homepage, loginFragment).commit();
+                Intent intent = new Intent(LaunchPage.this, LoginPage.class);
+                startActivity(intent);
             }
         });
 
